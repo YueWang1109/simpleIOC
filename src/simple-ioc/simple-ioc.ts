f96a8e7e24d884b5457c0f1389894ea205336258
Yue Wang <yuewang@Lulus-MacBook-Pro.local>
2021-07-18 17:21:00 -0400

    feat: add support for bingding classes

--- src/simple-ioc/simple-ioc.ts
+++ src/simple-ioc/simple-ioc.ts
@@ -1,18 +1,40 @@
+interface IValue<T> {
+  obj?: T;
+  cls?: IClass<T>;
+}
+interface IClass<T> {
+  new (...args: any[]): T;
+}
+
 class SimpleIoc {
-  private _repo: Map<symbol, any>;
+  private _repo: Map<symbol, IValue<any>>;
   constructor() {
-    this._repo = new Map<symbol, any>();
+    this._repo = new Map<symbol, IValue<any>>();
   }
-  bind(key: symbol, value: any): void {
+  bindObject<T>(key: symbol, obj: T): void {
     if (this._repo.has(key)) {
       this._repo.delete(key);
     }
+    const value: IValue<T> = {obj};
     this._repo.set(key, value);
   }
-  use(key: symbol) {
-    return this._repo.get(key);
+  bindClass<T>(key: symbol, cls: IClass<T>, isSingle: boolean = true): void {
+    if (this._repo.has(key)) {
+      this._repo.delete(key);
+    }
+    const value: IValue<T> = {cls};
+    this._repo.set(key, value);
   }
-  remove(key: symbol): boolean {
+  use<T>(key: symbol): T | undefined {
+    const value = this._repo.get(key) as IValue<T>;
+    if (!value) return;
+    if (value.cls) {
+      value.obj = new value.cls();
+    }
+
+    return value.obj;
+  }
+  remove<T>(key: symbol): boolean {
     return this._repo.delete(key);
   }
 }
