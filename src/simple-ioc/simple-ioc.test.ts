cd0cf85994edd0099f65ee4572739416dbd34c40
Yue Wang <yuewang@Lulus-MacBook-Pro.local>
2021-07-18 17:21:52 -0400

    test: update test to check class binding

--- src/simple-ioc/simple-ioc.test.ts
+++ src/simple-ioc/simple-ioc.test.ts
@@ -2,12 +2,21 @@ import SimpleIoc from "./simple-ioc";
 
 describe("Simple IoC tests", () => {
   let container: SimpleIoc;
-  const testKey = Symbol.for("test");
 
-  class TestClass {
-    count = 0;
+  const myKey = Symbol.for("example");
+
+  interface ObjType {
+    name: string;
+    age: number;
+  }
+
+  interface ClassType {
+    sayHi: () => string;
+  }
+
+  class TestClass implements ClassType {
     sayHi() {
-      return `Hello, world! count = ${this.count++}`;
+      return `Hello, world!`;
     }
   }
 
@@ -16,48 +25,44 @@ describe("Simple IoC tests", () => {
   });
   describe("register dependencies", () => {
     it("bind objects", () => {
-      container.bind(testKey, {name: "Alex", age: 28});
-      expect(container.use(testKey)?.name).toEqual("Alex");
-      expect(container.use(testKey)?.age).toEqual(28);
+      container.bindObject<ObjType>(myKey, {name: "Alex", age: 28});
+      expect(container.use<ObjType>(myKey)?.name).toEqual("Alex");
+      expect(container.use<ObjType>(myKey)?.age).toEqual(28);
     });
     it("rebind objects", () => {
-      container.bind(testKey, {name: "Alex", age: 28});
-      expect(container.use(testKey)?.name).toEqual("Alex");
-      expect(container.use(testKey)?.age).toEqual(28);
+      container.bindObject<ObjType>(myKey, {name: "Alex", age: 28});
+      expect(container.use<ObjType>(myKey)?.name).toEqual("Alex");
+      expect(container.use<ObjType>(myKey)?.age).toEqual(28);
 
-      container.bind(testKey, {name: "bob", age: 35});
-      expect(container.use(testKey)?.name).toEqual("bob");
-      expect(container.use(testKey)?.age).toEqual(35);
+      container.bindObject<ObjType>(myKey, {name: "bob", age: 35});
+      expect(container.use<ObjType>(myKey)?.name).toEqual("bob");
+      expect(container.use<ObjType>(myKey)?.age).toEqual(35);
     });
 
     it("bind class", () => {
-      container.bind(testKey, new TestClass());
-      expect(container.use(testKey)?.sayHi()).toEqual("Hello, world! count = 0");
-      expect(container.use(testKey)?.sayHi()).toEqual("Hello, world! count = 1");
-      expect(container.use(testKey)?.sayHi()).toEqual("Hello, world! count = 2");
+      container.bindClass<ClassType>(myKey, TestClass, true);
+      expect(container.use<ClassType>(myKey)?.sayHi()).toEqual("Hello, world!");
     });
   });
 
   describe("remove dependencies", () => {
     it("remove objects", () => {
-      container.bind(testKey, {name: "Alex", age: 28});
-      expect(container.use(testKey)?.name).toEqual("Alex");
-      expect(container.use(testKey)?.age).toEqual(28);
+      container.bindObject<ObjType>(myKey, {name: "Alex", age: 28});
+      expect(container.use<ObjType>(myKey)?.name).toEqual("Alex");
+      expect(container.use<ObjType>(myKey)?.age).toEqual(28);
 
-      expect(container.remove(testKey)).toBe(true);
-      expect(container.use(testKey)).toBeUndefined();
-      expect(container.remove(testKey)).toBe(false);
+      expect(container.remove(myKey)).toBe(true);
+      expect(container.use<ObjType>(myKey)).toBeUndefined();
+      expect(container.remove(myKey)).toBe(false);
     });
 
     it("remove class", () => {
-      container.bind(testKey, new TestClass());
-      expect(container.use(testKey)?.sayHi()).toEqual("Hello, world! count = 0");
-      expect(container.use(testKey)?.sayHi()).toEqual("Hello, world! count = 1");
-      expect(container.use(testKey)?.sayHi()).toEqual("Hello, world! count = 2");
+      container.bindClass<ClassType>(myKey, TestClass);
+      expect(container.use<ClassType>(myKey)?.sayHi()).toEqual("Hello, world!");
 
-      expect(container.remove(testKey)).toBe(true);
-      expect(container.use(testKey)).toBeUndefined();
-      expect(container.remove(testKey)).toBe(false);
+      expect(container.remove(myKey)).toBe(true);
+      expect(container.use<ObjType>(myKey)).toBeUndefined();
+      expect(container.remove(myKey)).toBe(false);
     });
   });
 });
