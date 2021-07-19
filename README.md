9f8d34bad16f581c451cdd482daaa5d811e02613
Yue Wang <yuewang@Lulus-MacBook-Pro.local>
2021-07-19 00:08:48 -0400

    docs: add comments and readme file

---
+++ README.md
@@ -0,0 +1,25 @@
+# NBCUniversal Coding Challenge -- SimpleIOC Library
+
+Using Typescript to build simple IoC library
+
+**I've covered unit tests for those main use cases**
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `yarn`
+
+Install all npm dependencies before start the project
+
+### `yarn test --coverage`
+
+Launches the test runner in the interactive watch mode.
+
+### `yarn build`
+
+transcode the app to the `dist` folder.
+
+## TODO
+
+Simplify the injection by supporting decorator
--- src/simple-ioc/simple-ioc.ts
+++ src/simple-ioc/simple-ioc.ts
@@ -8,12 +8,18 @@ interface IClass<T> {
 }
 
 class SimpleIoc {
-  private _repo: Map<symbol, IValue<any>>;
-  private _isCreating: Map<symbol, IValue<any>>;
+  private _repo: Map<symbol, IValue<any>>; //use this map to store all registered objects/classes
+  private _isCreating: Map<symbol, IValue<any>>; // all on creating dependencies are put into this map to detect circular dependency
+
   constructor() {
     this._repo = new Map<symbol, IValue<any>>();
     this._isCreating = new Map<symbol, IValue<any>>();
   }
+  /**
+   * use bindObject to bind any variables (number/string/array/object)
+   * @param key a unique symbol as the key
+   * @param obj registered dependent object
+   */
   bindObject<T>(key: symbol, obj: T): void {
     if (this._repo.has(key)) {
       this._repo.delete(key);
@@ -21,6 +27,13 @@ class SimpleIoc {
     const value: IValue<T> = {obj, singleton: true};
     this._repo.set(key, value);
   }
+
+  /**
+   * use bindClass to register class into the container
+   * @param key a unique symbol as the key
+   * @param cls the registered dependent class
+   * @param isSingle settings for singleton
+   */
   bindClass<T>(key: symbol, cls: IClass<T>, isSingle: boolean = true): void {
     if (this._repo.has(key)) {
       this._repo.delete(key);
@@ -28,6 +41,14 @@ class SimpleIoc {
     const value: IValue<T> = {cls, singleton: isSingle};
     this._repo.set(key, value);
   }
+
+  /**
+   *
+   * @param key a unique symbol as the key
+   * @returns the correspond dependencies or undefined if not found
+   * this use method also helps to detect circular dependencies in constructor.
+   * a single dependency will be returned if the singleton is true.
+   */
   use<T>(key: symbol): T | undefined {
     const value = this._repo.get(key) as IValue<T>;
     if (!value) return;
@@ -49,6 +70,11 @@ class SimpleIoc {
       return result;
     }
   }
+  /**
+   *
+   * @param key
+   * @returns a boolean to indicate status of deleting the dependency
+   */
   remove<T>(key: symbol): boolean {
     return this._repo.delete(key);
   }
